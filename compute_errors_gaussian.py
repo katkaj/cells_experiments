1d479f37e83234f9cce13e1dd731e9bc8cedacdb
michalnand <michal.nand@gmail.com>
2019-03-26 06:02:12 +0100

    code cleaned up

---
+++ compute_errors_gaussian.py
@@ -0,0 +1,98 @@
+import json
+import numpy
+import tensor_load
+
+import libs_compute_errors
+
+def save_results(file_name_prefix, json_result):
+
+    json_file_name = file_name_prefix + ".json"
+
+    json_out_file = open(json_file_name, "w")
+    json.dump(json_result, json_out_file)
+
+
+#program starts here :
+
+load_start_offset = 800
+load_reshaped = True
+
+result_path = "/home/michal/programming/cells_results/"
+
+#load target values -> ground truth (real trajectory)
+print("loading target")
+target_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/target.json", load_start_offset, load_reshaped)
+
+target_tensor.print_info()
+
+
+
+print("loading experiments")
+
+experiment_disc8x8x3_net_4_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_4.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_4_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_4_depth.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_5_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_5.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_5_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_5_depth.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_6_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_6.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_6_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_6_depth.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_7_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_7.json", load_start_offset, load_reshaped)
+experiment_disc8x8x3_net_7_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc8x8x3/net_7_depth.json", load_start_offset, load_reshaped)
+
+experiment_disc16x16x3_net_4_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_4.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_4_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_4_depth.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_5_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_5.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_5_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_5_depth.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_6_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_6.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_6_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_6_depth.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_7_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_7.json", load_start_offset, load_reshaped)
+experiment_disc16x16x3_net_7_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc16x16x3/net_7_depth.json", load_start_offset, load_reshaped)
+
+experiment_disc40x20x3_net_4_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_4.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_4_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_4_depth.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_5_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_5.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_5_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_5_depth.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_6_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_6.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_6_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_6_depth.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_7_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_7.json", load_start_offset, load_reshaped)
+experiment_disc40x20x3_net_7_depth_tensor = tensor_load.TensorLoad(result_path + "trajectory_result/disc40x20x3/net_7_depth.json", load_start_offset, load_reshaped)
+
+print()
+print("computing errors")
+print()
+
+json_result = {}
+json_result["results"] = []
+
+
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_4_tensor.get(), "disc8x8x3_net_4"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_4_depth_tensor.get(), "disc8x8x3_net_4_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_5_tensor.get(), "disc8x8x3_net_5"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_5_depth_tensor.get(), "disc8x8x3_net_5_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_6_tensor.get(), "disc8x8x3_net_6"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_6_depth_tensor.get(), "disc8x8x3_net_6_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_7_tensor.get(), "disc8x8x3_net_7"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc8x8x3_net_7_depth_tensor.get(), "disc8x8x3_net_7_depth"))
+
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_4_tensor.get(), "disc16x16x3_net_4"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_4_depth_tensor.get(), "disc16x16x3_net_4_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_5_tensor.get(), "disc16x16x3_net_5"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_5_depth_tensor.get(), "disc16x16x3_net_5_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_6_tensor.get(), "disc16x16x3_net_6"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_6_depth_tensor.get(), "disc16x16x3_net_6_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_7_tensor.get(), "disc16x16x3_net_7"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc16x16x3_net_7_depth_tensor.get(), "disc16x16x3_net_7_depth"))
+
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_4_tensor.get(), "disc40x20x3_net_4"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_4_depth_tensor.get(), "disc40x20x3_net_4_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_5_tensor.get(), "disc40x20x3_net_5"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_5_depth_tensor.get(), "disc40x20x3_net_5_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_6_tensor.get(), "disc40x20x3_net_6"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_6_depth_tensor.get(), "disc40x20x3_net_6_depth"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_7_tensor.get(), "disc40x20x3_net_7"))
+json_result["results"].append(libs_compute_errors.compute_errors(target_tensor.get(), experiment_disc40x20x3_net_7_depth_tensor.get(), "disc40x20x3_net_7_depth"))
+
+print("saving results")
+save_results("trajectory_result/errors_gaussian", json_result)
+
+
+print("program done")
--- network_train.py
+++ network_train.py
@@ -1,7 +1,6 @@
 import sys
 import libs_python.pyphy as pyphy
 
-
 #1, load data from dats files and create motion tensor with normalised columns
 
 #load training data
@@ -10,26 +9,57 @@ training_dats_to_motion_tensor = pyphy.DatsToMotionTensor("parameters/training_d
 #load testing data, for normalisation use range from testing
 testing_dats_to_motion_tensor = pyphy.DatsToMotionTensor("parameters/testing_dats.json", "parameters/motion_tensor.json", training_dats_to_motion_tensor.tensor())
 
-#2, create network input making class - TensorSpatial
-training_tensor = pyphy.TensorSpatial("parameters/spatial_tensor.json", training_dats_to_motion_tensor.tensor())
-testing_tensor  = pyphy.TensorSpatial("parameters/spatial_tensor.json", testing_dats_to_motion_tensor.tensor())
 
-'''
-#2, create network input making class - TensorNoSpatial
-training_tensor = pyphy.TensorNoSpatial("parameters/no_spatial_tensor.json", training_dats_to_motion_tensor.tensor())
-testing_tensor  = pyphy.TensorNoSpatial("parameters/no_spatial_tensor.json", testing_dats_to_motion_tensor.tensor())
-'''
+def experiment(tensor_config, network_config):
 
-testing_count = 20000
-#3, create dataset
-dataset = pyphy.DatasetTrajectory(training_tensor, training_tensor, testing_count)
+    #2, create network input making class - TensorSpatial
+    training_tensor = pyphy.TensorSpatial(tensor_config, training_dats_to_motion_tensor.tensor())
+    testing_tensor  = pyphy.TensorSpatial(tensor_config, testing_dats_to_motion_tensor.tensor())
 
 
+    #3, create dataset
+    testing_count = 20000
+    dataset = pyphy.DatasetTrajectoryRuntime(training_tensor, training_tensor, testing_count)
 
-#4, run experiments, train network
+    #4, run experiments, train network
+    experiment = pyphy.RegressionExperiment(dataset, network_config)
+    experiment.run()
 
-experiment_12 = pyphy.RegressionExperiment(dataset, "networks/experiment_empty/")
-experiment_12.run()
+#8x8x3 discretisation, no gaussian kernel, no depth + depth
+experiment("parameters/disc8x8x3/spatial_tensor_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_4/")
+experiment("parameters/disc8x8x3/spatial_tensor_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_5/")
+experiment("parameters/disc8x8x3/spatial_tensor_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_6/")
+experiment("parameters/disc8x8x3/spatial_tensor_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_7/")
+
+experiment("parameters/disc8x8x3/spatial_tensor_depth_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_4_depth/")
+experiment("parameters/disc8x8x3/spatial_tensor_depth_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_5_depth/")
+experiment("parameters/disc8x8x3/spatial_tensor_depth_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_6_depth/")
+experiment("parameters/disc8x8x3/spatial_tensor_depth_no_gaussian.json","networks/disc8x8x3_no_gaussian/net_7_depth/")
+
+
+#16x16x3 discretisation, no gaussian kernel, no depth + depth
+experiment("parameters/disc16x16x3/spatial_tensor_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_4/")
+experiment("parameters/disc16x16x3/spatial_tensor_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_5/")
+experiment("parameters/disc16x16x3/spatial_tensor_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_6/")
+experiment("parameters/disc16x16x3/spatial_tensor_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_7/")
+
+experiment("parameters/disc16x16x3/spatial_tensor_depth_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_4_depth/")
+experiment("parameters/disc16x16x3/spatial_tensor_depth_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_5_depth/")
+experiment("parameters/disc16x16x3/spatial_tensor_depth_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_6_depth/")
+experiment("parameters/disc16x16x3/spatial_tensor_depth_no_gaussian.json","networks/disc16x16x3_no_gaussian/net_7_depth/")
+
+
+#40x20x3 discretisation, no gaussian kernel, no depth + depth
+experiment("parameters/disc40x20x3/spatial_tensor_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_4/")
+experiment("parameters/disc40x20x3/spatial_tensor_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_5/")
+experiment("parameters/disc40x20x3/spatial_tensor_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_6/")
+experiment("parameters/disc40x20x3/spatial_tensor_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_7/")
+
+experiment("parameters/disc40x20x3/spatial_tensor_depth_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_4_depth/")
+experiment("parameters/disc40x20x3/spatial_tensor_depth_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_5_depth/")
+experiment("parameters/disc40x20x3/spatial_tensor_depth_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_6_depth/")
+experiment("parameters/disc40x20x3/spatial_tensor_depth_no_gaussian.json","networks/disc40x20x3_no_gaussian/net_7_depth/")
+
 
 
 print("program done")
